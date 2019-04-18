3af475623612ff520574445b294b21a37ebdc61d
Robert Cabrera <robertcabrera4871@gmail.com>
2019-04-17 20:12:59 -0400

    basic moving

--- Dungeon Game/src/view/LevelView.java
+++ Dungeon Game/src/view/LevelView.java
@@ -1,29 +1,62 @@
 package view;
 
+import com.sun.javafx.css.StyleCache.Key;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class LevelView {
+	Pane root;
 	GridPane pane;
 	Scene scene;
+	Circle circle;
 
 	public LevelView() {
 		pane = new GridPane();
+		root = new Pane();
+
 		pane.setAlignment(Pos.CENTER);
-		pane.setPadding(new Insets(10,10,10,10));
+		pane.setBackground(createBackground());
+
+		addRectangles(createRectangles());
+		pane.setGridLinesVisible(true);
+
+		circle = createCircle();
+		root.getChildren().add(circle);
+		// root.getChildren().add(pane);
+		scene = new Scene(root, 700, 700);
+//		scene.setOnKeyPressed(e -> {
+//			KeyCode key = e.getCode();
+//			switch (key) {
+//			case W: circle.setCenterY(circle.getCenterY() - 5); break;
+//			case S: circle.setCenterY(circle.getCenterY() + 5); break;
+//			case A: circle.setCenterX(circle.getCenterX() - 5); break;
+//			case D: circle.setCenterX(circle.getCenterX() + 5); break;
+//			}
+//		});
+	}
+
+	public Circle createCircle() {
+		Circle circle = new Circle(20, Paint.valueOf("red"));
+		circle.setCenterX(200);
+		circle.setCenterY(200);
+		return circle;
+	}
+
+	public Background createBackground() {
 		BackgroundFill fill = new BackgroundFill(Paint.valueOf("#B8860B"), new CornerRadii(10.0), new Insets(10));
-		Background background = new Background(fill);
-		pane.setBackground(background);
-		Rectangle[] rectangles = createRectangles();
-		addRectangles(rectangles);
-		scene = new Scene(pane, 700, 700);
+		return new Background(fill);
 	}
 
 	public Rectangle[] createRectangles() {
@@ -31,7 +64,6 @@ public class LevelView {
 		for (int i = 0; i < 100; i++) {
 			rectangles[i] = new Rectangle(65, 65);
 			rectangles[i].setFill(Paint.valueOf("#A9A9A9"));
-			rectangles[i].setStroke(Paint.valueOf("#000000"));
 		}
 		return rectangles;
 	}
